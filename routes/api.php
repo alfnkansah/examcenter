02dbc79a844495050e50f01918f76db116c8bb5c
lynapps <obimpehjohn1@gmail.com>
2024-06-13 08:19:39 +0000

    started the integration of the ussd application for the system

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::controller(UssdMachineController::class)->group(function () {
-    Route::post('ussd-webhook-request', 'startUSSD')->name('ussd.webhook');
-});
